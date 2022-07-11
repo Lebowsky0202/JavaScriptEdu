4e0b29a33fdeb0f813094c8647769011d65228a7
Lebowsky0202 <tipoproger@gmail.com>
2022-07-11 06:48:45 +0600

    Курсы learn javascript - логические операторы

--- js/script.js
+++ js/script.js
@@ -1,43 +1,22 @@
 'use strict';
 
-let money = +prompt("Ваш бюджет на месяц?", "");
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+// Ввод пароля Администратора 
+// Курс learn.javascript
 
-const appData = {
-    buget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: null,
-    income: null,
-    savings: false,
-};
+let welk = prompt("Кто ты воин", "");
 
 
-for(let i = 0; i < 2; i++){
-    let a = prompt("Введите обязательную статью расходов: ", ""),
-        b = prompt("Во сколько это обойдется");
-
-    if (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50){
-        console.log("Работает");
-        appData.expenses[a] = b;
-    }else{
-        console.log("None");
+if(welk === "Админ"){
+    let pass = prompt("Пароль", "");
+    if (pass === "Я Главный"){
+        alert("Здравствуйте!");
+    }else if(pass == null){
+        alert("Отменено");
+    }else if(pass !== "Я Главный"){
+        alert("Неверный пароль!");
     }
-    
+}else if(welk == null){
+    alert("Отменено");
+}else if(welk !== "Админ"){
+    alert("Я не знаю кто ты");
 }
-
-appData.moneyPerDay = appData.buget / 30;
-
-alert(`Ежедневный доход составляет ${appData.moneyPerDay}`);
-
-if (appData.moneyPerDay < 100){
-    console.log("Минимальный уровень достатка");
-}else if(appData.moneyPerDay < 10000 && appData.moneyPerDay > 100){
-    console.log("Средний уровень достатка");
-}else if(appData.moneyPerDay > 10000){
-    console.log("Высокий уровень достатка");
-}else{
-    console.log("Ошибка");
-}
-
-//Задание 3 курс Ивана Петриченко
