99a81a4f580f4d71230d9776c9fa747f0018ecc6
Lebowsky0202 <tipoproger@gmail.com>
2022-07-15 00:49:45 +0600

    функции в js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
 'use strict';
 
+// Функций
 
-// Конструкция switch
-
-
-// Пример 1
-// let browser = prompt("Выбери браузер", "");
-
-// if (browser === 'Edge'){
-//     alert( "You've got the Edge!" );
-// }else if(browser === 'Chrome' 
-//     || browser === 'Firefox' 
-//     || browser === 'Safari' 
-//     || browser === 'Opera'){
-//     alert( 'Okay we support these browsers too' );
-// }else{
-//     alert( 'We hope that this page looks ok!' );
+// Первый вариант
+// function checkAge(age) {
+//     age = age > 18 ?  true : confirm('Родители разрешили?');
+//     return age;
 // }
 
-//Пример 2
+// Второй вариант
+// function checkAge(age) {
+//     age = age > 18 || confirm('Родители разрешили?');
+//     return age;
+// }
 
-const number = +prompt('Введите число между 0 и 3', '');
+// alert(checkAge(10));
 
-switch(number){
-    case 0:
-        alert('Вы ввели число 0');
-        break;
-    case 1:
-        alert('Вы ввели число 1');
-        break;
-    case 2:
-    case 3:
-        alert('Вы ввели число 2, а может и 3');
-        break;
+//Lesson 2\
+
+// function min(a, b){
+//     return a < b ? a : b;
+// }
+
+// console.log(min(3, -1));
+
+// lesson 3
+
+function pow(x, n){
+    let y = x;
+    for(let i = 1; i < n; i++){
+        x = x * y;
+    }
+    return x;
 }
+
+let x = +prompt("Введи значение x", "");
+let n = +prompt("Введи значение n", "");
+
+alert(`${x} в степени ${n} будет ${pow(x, n)}`);
