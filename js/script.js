70e86939709ac1a6ab6135674704c2071d920aa5
Lebowsky0202 <tipoproger@gmail.com>
2022-07-26 23:33:06 +0600

    Могучий мститель

--- js/script.js
+++ js/script.js
@@ -1,40 +1,49 @@
 'use strict';
+// Объекты 
+let Name = prompt("Как вас зовут", "Брюс Беннер");
+let secCode = prompt("Код доступа", "");
 
-// Функций
-
-// Первый вариант
-// function checkAge(age) {
-//     age = age > 18 ?  true : confirm('Родители разрешили?');
-//     return age;
-// }
-
-// Второй вариант
-// function checkAge(age) {
-//     age = age > 18 || confirm('Родители разрешили?');
-//     return age;
-// }
-
-// alert(checkAge(10));
-
-//Lesson 2\
-
-// function min(a, b){
-//     return a < b ? a : b;
-// }
-
-// console.log(min(3, -1));
-
-// lesson 3
-
-function pow(x, n){
-    let y = x;
-    for(let i = 1; i < n; i++){
-        x = x * y;
+function checkSec(name){
+    name = Name;
+    if (name === "Тор"){
+        verifyThor(secCode);
+    }else if (name === "Халк" || name === "Брюс Беннер"){
+        verifyHulk(secCode);
+    }else{
+        alert("Доступ запрещен (Имя)");
     }
-    return x;
 }
 
-let x = +prompt("Введи значение x", "");
-let n = +prompt("Введи значение n", "");
+function verifyThor(securityCode){
+    if(securityCode === "Златовласка"){
+        alert("Доступ разрешен");
+        alert("Здравствуйте! Златовласка");
+    }
+    else{
+        alert("Доступ запрещен");
+    }
+}
 
-alert(`${x} в степени ${n} будет ${pow(x, n)}`);
+function verifyHulk(securityCode){
+    if(securityCode === "Брюс Беннер" || securityCode === "Могучий мститель"){
+        alert("Доступ разрешен");
+        alert("Здравствуйте! Могучий мститель");
+    }
+    else{
+        alert("Доступ запрещен");
+    }
+}
+
+let thor = {
+    name: "Тор",
+    age: 1500,
+    secreetKey: "Златовласка",
+};
+
+let hulk = {
+    name: "Брюс Беннер",
+    age: 47,
+    secreetKey: "Могучий мститель",
+}
+
+checkSec(Name);
