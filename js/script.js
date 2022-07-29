872c52d05fdfbac778b7673fb6d8104bf7e447b3
Lebowsky0202 <tipoproger@gmail.com>
2022-07-29 14:33:37 +0600

    Каркулятор

--- js/script.js
+++ js/script.js
@@ -1,49 +1,25 @@
 'use strict';
-// Объекты 
-let Name = prompt("Как вас зовут", "Брюс Беннер");
-let secCode = prompt("Код доступа", "");
 
-function checkSec(name){
-    name = Name;
-    if (name === "Тор"){
-        verifyThor(secCode);
-    }else if (name === "Халк" || name === "Брюс Беннер"){
-        verifyHulk(secCode);
-    }else{
-        alert("Доступ запрещен (Имя)");
-    }
-}
+//Методы объекта this
+// Каркулятор
 
-function verifyThor(securityCode){
-    if(securityCode === "Златовласка"){
-        alert("Доступ разрешен");
-        alert("Здравствуйте! Златовласка");
-    }
-    else{
-        alert("Доступ запрещен");
-    }
-}
+let calculator = {
+    a: 0,
+    b: 0,
+    read (){
+        this.a = +prompt("Первое число");
+        this.b = +prompt("Второе число");
+    },
 
-function verifyHulk(securityCode){
-    if(securityCode === "Брюс Беннер" || securityCode === "Могучий мститель"){
-        alert("Доступ разрешен");
-        alert("Здравствуйте! Могучий мститель");
-    }
-    else{
-        alert("Доступ запрещен");
-    }
-}
+    sum (){
+        return this.a + this.b;
+    },
 
-let thor = {
-    name: "Тор",
-    age: 1500,
-    secreetKey: "Златовласка",
+    mul (){
+        return this.a * this.b;
+    }
 };
 
-let hulk = {
-    name: "Брюс Беннер",
-    age: 47,
-    secreetKey: "Могучий мститель",
-}
-
-checkSec(Name);
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
